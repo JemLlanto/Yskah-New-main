8737b651341c3de0f4bc1f866c1be5464c0d3952
JemLlanto <llantojem@gmail.com>
2024-06-18 15:32:47 +0800

    asd
    aa

--- update.php
+++ update.php
@@ -1,23 +1,24 @@
 <?php
- include ("connection.php");
-  if(count($_POST) > 0) {
-      $first_name = $_POST['first_name'];
-      $last_name = $_POST['last_name'];
-      $sex = $_POST['sex'];
-      $phone = $_POST['phone'];
-      $username = $_POST['username'];
-      $address = $_POST['address'];
-      $city = $_POST['city'];
-      $province = $_POST['province'];
-      $zip = $_POST['zip'];
-      $email = $_POST['email'];
-      $password = $_POST['password'];
+include ("connection.php");
+if (count($_POST) > 0) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $sex = $_POST['sex'];
+    $phone = $_POST['phone'];
+    $username = $_POST['username'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $province = $_POST['province'];
+    $zip = $_POST['zip'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-      // $result = mysqli_query($conn, "SELECT * FROM user_table WHERE id='" . $_POST['id'] . "'");
-      // $row = $result->fetch_assoc();
-      // $hashed_password = $row['password'];
+    // $result = mysqli_query($conn, "SELECT * FROM user_table WHERE id='" . $_POST['id'] . "'");
+    // $row = $result->fetch_assoc();
+    // $hashed_password = $row['password'];
+//as
 
-      // $password = !empty($_POST['password']) ? $_POST['password'] : $hashed_password;
+    // $password = !empty($_POST['password']) ? $_POST['password'] : $hashed_password;
 
     mysqli_query($conn, "UPDATE user_table SET
       first_name='$first_name',
@@ -35,11 +36,11 @@
     alert('Record Successfully modified');
     window.location='user_table.php';
     </script>";
-  }
+}
 
-  $result = mysqli_query($conn, "SELECT * FROM user_table WHERE id='" . $_GET['id'] . "'");
-  $row = mysqli_fetch_array($result);
- ?>
+$result = mysqli_query($conn, "SELECT * FROM user_table WHERE id='" . $_GET['id'] . "'");
+$row = mysqli_fetch_array($result);
+?>
 
 
 
@@ -64,12 +65,12 @@
                     <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
                     <div class="form-floating mb-3 w-50 ">
                         <input type="text" class="form-control" id="firstName" name="first_name" placeholder="John"
-                            value="<?php echo $row['first_name'];?>" />
+                            value="<?php echo $row['first_name']; ?>" />
                         <label for="firstName" class="form-label text-secondary">First name</label>
                     </div>
                     <div class="form-floating mb-3 w-50">
                         <input type="text" class="form-control" id="lastname" name="last_name" placeholder="Doe"
-                            value="<?php echo $row['last_name'];?>" />
+                            value="<?php echo $row['last_name']; ?>" />
                         <label for="lastName" class="form-label text-secondary">Last name</label>
                     </div>
                 </div>
@@ -85,12 +86,12 @@
 
                 <div class="form-floating mb-3 w-100">
                     <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com"
-                        value="<?php echo $row['email'];?>" />
+                        value="<?php echo $row['email']; ?>" />
                     <label for="email" class="form-label text-secondary">Email</label>
                 </div>
                 <div class="form-floating mb-3">
                     <input type="text" class="form-control" id="phone" name="phone" placeholder="63+"
-                        value="<?php echo $row['phone'];?>" />
+                        value="<?php echo $row['phone']; ?>" />
                     <label for="phone" class="form-label text-secondary">Phone</label>
                 </div>
 
@@ -117,12 +118,12 @@
                 <div class="d-flex gap-1">
                     <div class="form-floating mb-3 w-50">
                         <input type="text" class="form-control" id="username" placeholder="username" name="username"
-                            value="<?php echo $row['username'];?>" />
+                            value="<?php echo $row['username']; ?>" />
                         <label for="username" class="form-label text-secondary">Username</label>
                     </div>
                     <div class="form-floating mb-3 w-50">
                         <input type="password" class="form-control" id="password" placeholder="password" name="password"
-                            value="<?php echo $row['password'];?>" />
+                            value="<?php echo $row['password']; ?>" />
                         <label for="password" class="form-label text-secondary">Password</label>
                     </div>
                 </div>
